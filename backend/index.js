61b73b424051d86e353b0396af0d2d99916875fd
Ankur Kumar <56749949+Ak47-crypto@users.noreply.github.com>
2024-11-26 14:27:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors(
   }
 ))
 app.use(express.json())
-app.get('/',(res,req)=>{
+app.get('/',(req,res)=>{
   res.json({message:"welcomes you in inotes"})
 })
 app.use('/api/auth', require('./routes/Auth'))
